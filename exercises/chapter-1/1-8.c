6044c04c6df673b5b94631cddaa9bfc0df18de92
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-01-15 00:10:59 +0100

    style: Modify comment style in 1-8.c

--- exercises/chapter-1/1-8.c
+++ exercises/chapter-1/1-8.c
@@ -2,29 +2,31 @@
 
 /* Federico Maria Massari / federico.massari@bocconialumni.it */
 
-/* Exercise 1-8 p.20 */
-/* Write a program to count blanks, tabs, and newlines */
-/* Equality with EOF is reached when CTRL+D is pressed */
-/* Compile with cc -ansi 1-8.c and run with ./a.out */
+/*
+ * Exercise 1-8 p.20
+ * Write a program to count blanks, tabs, and newlines
+ * Equality with EOF is reached when CTRL+D is pressed
+ * Compile with cc -ansi 1-8.c and run with ./a.out
+ */
 
 main()
 {
     int blank, tab, newline;
     int c;
-    
+
     /* Initialise variables */
     blank = 0;
     tab = 0;
     newline = 0;
-    
+
     /* Increment variable if input != EOF and an equality is met */
     while ((c = getchar()) != EOF) {
         if (c == ' ')
             ++blank;
-    
+
         if (c == '\t')
             ++tab;
-            
+
         if (c == '\n')
             ++newline;
 }
