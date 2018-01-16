d60795f3c6f25c5f997df8d2e53d96e29b4e2f82
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-01-16 08:48:28 +0100

    style: Modify formatting of 1-08.c

--- exercises/chapter-1/1-08.c
+++ exercises/chapter-1/1-08.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
-
-/* Federico Maria Massari / federico.massari@bocconialumni.it */
-
 /*
- * Exercise 1-8 p.20
- * Write a program to count blanks, tabs, and newlines
- * Equality with EOF is reached when CTRL+D is pressed
- * Compile with cc -ansi 1-8.c and run with ./a.out
+ * Filename   1-08.c
+ * Author     Federico Maria Massari <federico.massari@bocconialumni.it>
+ * Book       "The C Programming Language, 2nd Ed.", by Kernighan, B.W.
+ *             and Ritchie, D.M.
+ *
+ * Exercise 1-8, p.20
+ * Write a program to count blanks, tabs, and newlines.
  */
+
+#include <stdio.h>
 
 main()
 {
     int blank, tab, newline;
-    int c;
+    int c;    /* int, as opposed to char, is big enough to hold EOF */
 
-    /* Initialise variables */
-    blank = 0;
+    blank = 0;    /* Initialise variables */
     tab = 0;
     newline = 0;
 
-    /* Increment variable if input != EOF and an equality is met */
+    /* Increment variable if input != EOF and an equality is met;
+     * equality with EOF is reached when CTRL+D is pressed. */
     while ((c = getchar()) != EOF) {
         if (c == ' ')
             ++blank;
@@ -30,7 +31,7 @@ main()
         if (c == '\n')
             ++newline;
     }
-    
+
     /* Print output after CTRL+D is pressed */
     printf("\nBlank count: %d\n", blank);
     printf("Tab count: %d\n", tab);
