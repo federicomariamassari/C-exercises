363f7b814ad845fb75696e92f7abdebf2809dae1
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-01-17 00:31:41 +0100

    feat: Add 1-01.c to repository

---
+++ exercises/chapter-1/1-01.c
@@ -0,0 +1,17 @@
+/*
+ * Filename   1-01.c
+ * Author     Federico Maria Massari <federico.massari@bocconialumni.it>
+ * Book       "The C Programming Language, 2nd Ed.", by Kernighan, B.W.
+ *             and Ritchie, D.M.
+ *
+ * Exercise 1-1, p.8
+ * Run the "hello, world" program on your system. Experiment with leaving
+ * out parts of the program, to see what error messages you get.
+ */
+
+#include <stdio.h>    /* Standard input-output library */
+
+main()    /* Execution begins with main; every program must have one */
+{
+    printf("hello, world\n");
+}
